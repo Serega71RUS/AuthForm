5de315c61ee5d88eb0f11273c806aa1f6c15c5cf
Serega71RUS <serega71rus@gmail.com>
2021-10-12 13:01:29 +0300

    Added reg.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 "use strict"
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', function() { //() =>
 
     //Login
     const txtBoxEmail = document.querySelector('[data-login]');
     const txtBoxPass = document.querySelector('[data-password]');
     const btn = document.querySelector('.btnlogin');
+    const parent = document.querySelector('.auth__inner');
     let chEmail = false;
     let chPass = false;
 
@@ -13,17 +14,21 @@ window.addEventListener('DOMContentLoaded', () => {
     function addErrorEmail() {
         const element = document.createElement('h1');
         element.classList.add('error_text');
+        element.id = "errEmail"; //IE
         element.textContent = "Вы ввели неккоректный адрес электронной почты";
-        txtBoxEmail.addEventListener('keyup', (e) => {
+        txtBoxEmail.addEventListener('keyup', function(e) { //(e) =>
             let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
             if(!reg.test(e.target.value)){
-                txtBoxEmail.after(element);
+                //txtBoxEmail.after(element);
+                txtBoxEmail.insertAdjacentElement('afterend', element);
                 chEmail = false;
                 BtnLogin();
             }
             else{
                 if(txtBoxEmail.nextElementSibling.className == "error_text"){
-                    txtBoxEmail.nextElementSibling.remove();
+                    //txtBoxEmail.nextElementSibling.remove();
+                    const el = document.getElementById('errEmail'); //IE
+                    parent.removeChild(el); //IE
                     chEmail = true;
                     BtnLogin();
                 }
@@ -35,18 +40,22 @@ window.addEventListener('DOMContentLoaded', () => {
     function addErrorPass() {
         const element = document.createElement('h1');
         element.classList.add('error_text');
+        element.id = "errPass"; //IE
         element.textContent = "Пароль должен быть не менее 7 и не более 20 символов";
-        txtBoxPass.addEventListener('keyup', (e) => {
+        txtBoxPass.addEventListener('keyup', function(e) { //(e) =>
             console.log(e.target.value.length);
             if(e.target.value.length <= 20 && e.target.value.length >= 7){
                 if(txtBoxPass.nextElementSibling.className == "error_text"){
-                    txtBoxPass.nextElementSibling.remove();
+                    //txtBoxPass.nextElementSibling.remove();
+                    const el = document.getElementById('errPass'); //IE
+                    parent.removeChild(el); //IE
                     chPass = true;
                     BtnLogin();
                 }
             }
             else{
-                txtBoxPass.after(element);
+                //txtBoxPass.after(element);
+                txtBoxPass.insertAdjacentElement('afterend', element);
                 chPass = false;
                 BtnLogin();
             }
@@ -54,7 +63,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     //Обработчик кнопки
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', function() { //() =>
         alert("Пользователь авторизован");
     });
 
@@ -71,15 +80,6 @@ window.addEventListener('DOMContentLoaded', () => {
             btn.removeAttribute('disabled');
         }
     }
-
-    //Password
-
-    
-    
-
-
-
-
 
     //Main
     addErrorEmail();
--- js/reg.js
+++ js/reg.js
@@ -1,66 +1,96 @@
 "use strict"
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', function() { //() =>
 
-    //Password
-    const txtBoxEmail = document.querySelector('[data-login]');
-    const txtBoxPass = document.querySelector('[data-password]');
-    const btn = document.querySelector('.btnlogin');
-    let chEmail = false;
-    let chPass = false;
+    //Login
+    const parent = document.querySelector('.auth__inner'); //IE
+    const txtBoxEmail = parent.querySelector('[data-login]');
+    const txtBoxPass = parent.querySelector('[data-password]');
+    const btn = parent.querySelector('.btnlogin');
+    let ch = {
+        FIO: false,
+        Email: false,
+        Pass: false,
+        Pass2: false
+    };
 
-    //Проверка Email
-    function addErrorEmail() {
+    //Добавление ошибки
+    function AddErrText(id, text, txtBox, func) {
         const element = document.createElement('h1');
         element.classList.add('error_text');
-        element.textContent = "Вы ввели неккоректный адрес электронной почты";
-        txtBoxEmail.addEventListener('keyup', (e) => {
-            let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-            if(!reg.test(e.target.value)){
-                txtBoxEmail.after(element);
-                chEmail = false;
+        element.id = id; //IE
+        element.textContent = text;
+        txtBox.addEventListener('keyup', function(e) {func(e, element, id, txtBox)});
+    }
+
+    //Проверка ФИО
+    function checkFIO(e, element, id, txtBox) {
+        let reg = /^[а-яА-Я]+/;
+        if(!reg.test(e.target.value)){
+            //txtBoxEmail.after(element);
+            txtBox.insertAdjacentElement('afterend', element);
+            ch.FIO = false;
+            BtnLogin();
+        }
+        else{
+            if(txtBox.nextElementSibling.id == id){
+                //txtBoxEmail.nextElementSibling.remove();
+                const el = document.getElementById(id); //IE
+                parent.removeChild(el); //IE
+                ch.FIO = true;
                 BtnLogin();
             }
-            else{
-                if(txtBoxEmail.nextElementSibling.className == "error_text"){
-                    txtBoxEmail.nextElementSibling.remove();
-                    chEmail = true;
-                    BtnLogin();
-                }
+        }
+    }
+
+    //Проверка Email
+    function checkEmail(e, element, id, txtBox){
+        let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if(!reg.test(e.target.value)){
+            //txtBoxEmail.after(element);
+            txtBox.insertAdjacentElement('afterend', element);
+            ch.Email = false;
+            BtnLogin();
+        }
+        else{
+            if(txtBox.nextElementSibling.id == id){
+                //txtBoxEmail.nextElementSibling.remove();
+                const el = document.getElementById(id); //IE
+                parent.removeChild(el); //IE
+                ch.Email = true;
+                BtnLogin();
             }
-        });
+        }
     }
 
     //Проверка пароля
-    function addErrorPass() {
-        const element = document.createElement('h1');
-        element.classList.add('error_text');
-        element.textContent = "Пароль должен быть не менее 7 и не более 20 символов";
-        txtBoxPass.addEventListener('keyup', (e) => {
-            console.log(e.target.value.length);
-            if(e.target.value.length <= 20 && e.target.value.length >= 7){
-                if(txtBoxPass.nextElementSibling.className == "error_text"){
-                    txtBoxPass.nextElementSibling.remove();
-                    chPass = true;
-                    BtnLogin();
-                }
-            }
-            else{
-                txtBoxPass.after(element);
-                chPass = false;
+    function checkPass(e, element, id, txtBox) {
+        console.log(e.target.value.length);
+        if(e.target.value.length <= 20 && e.target.value.length >= 7){
+            if(txtBox.nextElementSibling.id == id){
+                //txtBoxPass.nextElementSibling.remove();
+                const el = document.getElementById(id); //IE
+                parent.removeChild(el); //IE
+                ch.Pass = true;
                 BtnLogin();
             }
-        });
+        }
+        else{
+            //txtBoxPass.after(element);
+            txtBox.insertAdjacentElement('afterend', element);
+            ch.Pass = false;
+            BtnLogin();
+        }
     }
 
     //Обработчик кнопки
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', function() { //() =>
         alert("Пользователь авторизован");
     });
 
     //Проверка Кнопки
     function BtnLogin() {
-        if(chEmail == false || chPass == false){
+        if(ch.FIO == false || ch.Email == false || ch.Pass == false || ch.Pass2 == false){
             //btn.classList.toggle('hover');
             btn.classList.remove('hover');
             btn.setAttribute('disabled', true);
@@ -72,9 +102,27 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-
     //Main
-    addErrorEmail();
-    addErrorPass();
     BtnLogin();
+
+    AddErrText(
+        "errFIO",
+        "Допускаются только символы кириллицы",
+        parent.querySelector('[data-fio]'),
+        checkFIO
+    );
+
+    AddErrText(
+        "errEmail",
+        "Вы ввели неккоректный адрес электронной почты",
+        txtBoxEmail,
+        checkEmail
+    );
+
+    AddErrText(
+        "errPass",
+        "Пароль должен быть не менее 7 и не более 20 символов",
+        txtBoxPass,
+        checkPass
+    );
 });
